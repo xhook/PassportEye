325449051ef5e4a23e420d09a4284511bbaa1098
Dmitry Zhukov <dzhukov@transferwise.com>
2017-05-20 21:43:06 +0300

    clean up frontend code a bit

--- server/static/assets/js/fileUpload.js
+++ server/static/assets/js/fileUpload.js
@@ -1,75 +1,44 @@
-(function($, Dropzone){
+(function ($) {
 
-console.log("INVOKED fileUpload.js");
-console.log(Dropzone);
-// "myAwesomeDropzone" is the camelized version of the HTML element's ID
-// The recommended way from within the init configuration:
-Dropzone.options.myAwesomeDropzone = {
-    init: function() {
-        this.on("success", function(file, response) {
-            $("#json-output").html(JSON.stringify(response, null, 2));
-       });
-    }
-};
-})(jQuery, window.Dropzone);
-
-function readURL(input) {
-
-	if (input.files && input.files[0]) {
-		var reader = new FileReader();
-
-		reader.onload = function (e) {
-			$('#blah').attr('src', e.target.result);
-		};
-
-		reader.readAsDataURL(input.files[0]);
-	}
-}
-
-$(':file').on('change', function() {
-	var file = this.files[0];
-	if (file.size > 1024*1024*10) {
-		alert('max upload size is 1k')
+	function readURL(input) {
+		if (input.files && input.files[0]) {
+			var reader = new FileReader();
+			reader.onload = function (e) {
+				$('#blah').attr('src', e.target.result);
+			};
+			reader.readAsDataURL(input.files[0]);
+		}
 	}
 
-	readURL(this);
-	// Also see .name, .type
-});
-
-
-$(':button').on('click', function() {
-	$.ajax({
-		// Your server script to process the upload
-		url: 'upload_file',
-		type: 'POST',
-
-		// Form data
-		data: new FormData($('form')[0]),
-
-		// Tell jQuery not to process data or worry about content-type
-		// You *must* include these options!
-		cache: false,
-		contentType: false,
-		processData: false,
-		success: function (response) {
-			$('#json-output').html(JSON.stringify(response, null, 2));
+	$(':file').on('change', function () {
+		var file = this.files[0];
+		if (file.size > 1024 * 1024 * 10) {
+			alert('max upload size is 1k')
 		}
 
-		// Custom XMLHttpRequest
-		// xhr: function() {
-		// 	var myXhr = $.ajaxSettings.xhr();
-		// 	if (myXhr.upload) {
-		// 		For handling the progress of the upload
-				// myXhr.upload.addEventListener('progress', function(e) {
-				// 	if (e.lengthComputable) {
-				// 		$('progress').attr({
-				// 			value: e.loaded,
-				// 			max: e.total
-				// 		});
-				// 	}
-				// } , false);
-			// }
-			// return myXhr;
-		// }
+		readURL(this);
+		$.ajax({
+			// Your server script to process the upload
+			url: 'upload_file',
+			type: 'POST',
+
+			// Form data
+			data: new FormData($('form')[0]),
+
+			// Tell jQuery not to process data or worry about content-type
+			// You *must* include these options!
+			cache: false,
+			contentType: false,
+			processData: false,
+			success: function (response) {
+				$('#json-output').text(JSON.stringify(response, null, 4));
+			}
+		});
 	});
-});
+
+
+	$(':button').on('click', function () {
+	});
+
+})(jQuery, window.Dropzone);
+
