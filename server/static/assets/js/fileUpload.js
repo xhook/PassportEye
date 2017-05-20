6e6cefcdf3fc580e3693f0e0cb303655f841a7de
Dmitry Zhukov <dzhukov@transferwise.com>
2017-05-20 19:44:28 +0300

    New upload

--- server/static/assets/js/fileUpload.js
+++ server/static/assets/js/fileUpload.js
@@ -12,3 +12,64 @@ Dropzone.options.myAwesomeDropzone = {
     }
 };
 })(jQuery, window.Dropzone);
+
+function readURL(input) {
+
+	if (input.files && input.files[0]) {
+		var reader = new FileReader();
+
+		reader.onload = function (e) {
+			$('#blah').attr('src', e.target.result);
+		};
+
+		reader.readAsDataURL(input.files[0]);
+	}
+}
+
+$(':file').on('change', function() {
+	var file = this.files[0];
+	if (file.size > 1024*1024*10) {
+		alert('max upload size is 1k')
+	}
+
+	readURL(this);
+	// Also see .name, .type
+});
+
+
+$(':button').on('click', function() {
+	$.ajax({
+		// Your server script to process the upload
+		url: 'upload_file',
+		type: 'POST',
+
+		// Form data
+		data: new FormData($('form')[0]),
+
+		// Tell jQuery not to process data or worry about content-type
+		// You *must* include these options!
+		cache: false,
+		contentType: false,
+		processData: false,
+		success: function (response) {
+			$('#json-output').html(JSON.stringify(response, null, 2));
+		}
+
+		// Custom XMLHttpRequest
+		// xhr: function() {
+		// 	var myXhr = $.ajaxSettings.xhr();
+		// 	if (myXhr.upload) {
+		// 		For handling the progress of the upload
+				// myXhr.upload.addEventListener('progress', function(e) {
+				// 	if (e.lengthComputable) {
+				// 		$('progress').attr({
+				// 			value: e.loaded,
+				// 			max: e.total
+				// 		});
+				// 	}
+				// } , false);
+			// }
+			// return myXhr;
+		// }
+	});
+});
